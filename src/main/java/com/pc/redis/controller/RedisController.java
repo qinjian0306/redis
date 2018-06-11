0775e9664f6c9c2b8cf7682fc7452a64df1bec16
qinjian <qinjian0306@163.com>
2018-06-11 12:27:54 +0800

    redis pubsub

--- src/main/java/com/pc/redis/controller/RedisController.java
+++ src/main/java/com/pc/redis/controller/RedisController.java
@@ -12,6 +12,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class RedisController {
 
     @Autowired
+    private JedisCli jedisCli;
+
+    @Autowired
     private PubClient pubClient;
     @Autowired
     private SubClient subClient;
@@ -23,6 +26,17 @@ public class RedisController {
 
         pubClient.publish("pubsub test" , "msg : ----->>>  redis pub test");
 
+        return "pub";
+    }
+
+
+    @RequestMapping("/getpub")
+    public String getpub(){
+
+        jedisCli.setNoExpire("key1","pubsub");
+        System.out.println(jedisCli.get("key1"));
+
+        System.out.println(jedisCli.get("pubsub test"));  // 普通get方式，不能获取pubsub的数据 null
         return "pub";
     }
 
